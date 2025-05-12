9cfc975434cd20e2624810e2dfc7b28b5e8b9e58
Eder-TS <steder20@yahoo.com.br>
2025-05-12 09:52:54 -0300

    Feat: Update Product service.

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+import { Product } from '../database/entities/Product';
+import { productsRepositories } from '../database/repositories/ProductsRepositories';
+
+interface IUpdateProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+export default class UpdateProductService {
+  async excute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IUpdateProduct): Promise<Product> {
+    const productExistsById = await productsRepositories.findById(id);
+
+    if (!productExistsById) throw new AppError('Product not found', 404);
+
+    const productExistsByName = await productsRepositories.findByName(name);
+
+    if (productExistsByName && productExistsByName.id !== id)
+      throw new AppError('There is already one product with this name.', 409);
+
+    productExistsById.name = name;
+    productExistsById.price = price;
+    productExistsById.quantity = quantity;
+
+    const updateProduct = await productsRepositories.save(productExistsById);
+
+    return updateProduct;
+  }
+}
