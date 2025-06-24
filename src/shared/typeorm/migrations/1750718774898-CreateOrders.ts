60be06549ad0886064a564d135ff5a40a0293c98
Eder-TS <steder20@yahoo.com.br>
2025-06-23 22:31:19 -0300

    Feat: Migration orders.

---
+++ src/shared/typeorm/migrations/1750718774898-CreateOrders.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrders1750718774898 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
---
+++ src/shared/typeorm/migrations/1750719420106-AddCustomerIdToOrders.ts
@@ -0,0 +1,35 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddCustomerIdToOrders1750719420106 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customerId',
+        type: 'integer',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomer',
+        columnNames: ['customerId'],
+        referencedTableName: 'customers',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
+    await queryRunner.dropColumn('orders', 'customerId');
+  }
+}
