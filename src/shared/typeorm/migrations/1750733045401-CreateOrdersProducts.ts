a77aae23b8ab9078057fbe207359ad7e4702b43a
Eder-TS <steder20@yahoo.com.br>
2025-06-24 20:31:07 -0300

    Feat: OrdersProducts and references.

---
+++ src/shared/typeorm/migrations/1750733045401-CreateOrdersProducts.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrdersProducts1750728819187 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'ordersProducts',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('ordersProducts');
+  }
+}
